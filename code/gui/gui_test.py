e343caa319a0d668e178637ccf3816edab3c5b87
Alessandro Fugazza <137590109+alessandrofugazza@users.noreply.github.com>
2025-02-21 17:43:04 +0100

    fix bug in lambda function argument

--- code/gui/gui_test.py
+++ code/gui/gui_test.py
@@ -8,7 +8,7 @@ root.title("Remote Script Executor")
 
 connect_button = tk.Button(root, text="Establish Connection", command=ssh_manager.establish_connection)
 connect_button.pack()
-execute_button = tk.Button(root, text="Execute Remote Command", command=lambda: ssh_manager.execute_remote_command('source /home/shell/defaultenv/bin/activate; python3 /home/shell/synced_servos.py'))
+execute_button = tk.Button(root, text="Execute Remote Command", command=lambda: ssh_manager.execute_remote_command('synced_servos.py'))
 execute_button.pack()
 close_button = tk.Button(root, text="Close Connection", command=ssh_manager.close_connection)
 close_button.pack()
