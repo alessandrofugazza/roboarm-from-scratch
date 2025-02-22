d135e7a16d5e3b9c8c7087aec7a56a95ed0e5155
Alessandro Fugazza <137590109+alessandrofugazza@users.noreply.github.com>
2025-02-23 00:49:03 +0100

    add setting incremental jog and print status feedback in terminal

--- code/gui/preper_gui.py
+++ code/gui/preper_gui.py
@@ -1,37 +1,59 @@
 import tkinter as tk
+from tkinter import ttk
 from ssh_manager import SSHClientManager
 
+# TODO use iteration to add guiobjects
+
 ssh_manager = SSHClientManager()
+
+
+# def print_status():
+#     ssh_manager.send_command("a3a.show_status()")
+
 
 root = tk.Tk()
 root.title("A3A")
 
-connect_button = tk.Button(root, text="Establish Connection", command=ssh_manager.establish_connection)
-connect_button.pack()
+incremental_jog = tk.StringVar()
 
+
+
+incremental_jog_label = ttk.Label(root, text="Incremental Jog: ")
+incremental_jog_label.pack()
+incremental_jog_entry = ttk.Entry(root, width=2, textvariable=incremental_jog)
+incremental_jog_entry.pack()
 
 # this whole thing is ugly as sin
 
-execute_button = tk.Button(root, text="J1 -5", command=lambda: ssh_manager.send_command('a3a.move_joint_incremental(0, -1)'))
-execute_button.pack()
-execute_button = tk.Button(root, text="J1 +5", command=lambda: ssh_manager.send_command('a3a.move_joint_incremental(0, 1)'))
+execute_button = ttk.Button(root, text="STATUS", command=lambda: ssh_manager.send_command('a3a.show_status()'))
 execute_button.pack()
 
-execute_button = tk.Button(root, text="J2 -5", command=lambda: ssh_manager.send_command('a3a.move_joint_incremental(1, -1)'))
-execute_button.pack()
-execute_button = tk.Button(root, text="J2 +5", command=lambda: ssh_manager.send_command('a3a.move_joint_incremental(1, 1)'))
-execute_button.pack()
-
-execute_button = tk.Button(root, text="J3 -5", command=lambda: ssh_manager.send_command('a3a.move_joint_incremental(2, -1)'))
-execute_button.pack()
-execute_button = tk.Button(root, text="J3 +5", command=lambda: ssh_manager.send_command('a3a.move_joint_incremental(2, 1)'))
-execute_button.pack()
-
-execute_button = tk.Button(root, text="zero", command=lambda: ssh_manager.send_command('a3a.go_zero()'))
+execute_button = ttk.Button(root, text="Update Incremental Jog", command=lambda: ssh_manager.send_command(f'a3a.set_incremental_jog({incremental_jog.get()})'))
 execute_button.pack()
 
 
-close_button = tk.Button(root, text="Close Connection", command=ssh_manager.close_connection)
-close_button.pack()
+execute_button = ttk.Button(root, text=f"J1 -{incremental_jog.get()}", command=lambda: ssh_manager.send_command('a3a.move_joint_incremental(0, -1)'))
+execute_button.pack()
+execute_button = ttk.Button(root, text=f"J1 +{incremental_jog.get()}", command=lambda: ssh_manager.send_command('a3a.move_joint_incremental(0, 1)'))
+execute_button.pack()
+
+execute_button = ttk.Button(root, text=f"J2 -{incremental_jog.get()}", command=lambda: ssh_manager.send_command('a3a.move_joint_incremental(1, -1)'))
+execute_button.pack()
+execute_button = ttk.Button(root, text=f"J2 +{incremental_jog.get()}", command=lambda: ssh_manager.send_command('a3a.move_joint_incremental(1, 1)'))
+execute_button.pack()
+
+execute_button = ttk.Button(root, text=f"J3 -{incremental_jog.get()}", command=lambda: ssh_manager.send_command('a3a.move_joint_incremental(2, -1)'))
+execute_button.pack()
+execute_button = ttk.Button(root, text=f"J3 +{incremental_jog.get()}", command=lambda: ssh_manager.send_command('a3a.move_joint_incremental(2, 1)'))
+execute_button.pack()
+
+execute_button = ttk.Button(root, text="zero", command=lambda: ssh_manager.send_command('a3a.go_zero()'))
+execute_button.pack()
+
+connect_button = ttk.Button(root, text="Establish Connection", command=ssh_manager.establish_connection)
+connect_button.pack(side="left")
+
+close_button = ttk.Button(root, text="Close Connection", command=ssh_manager.close_connection)
+close_button.pack(side="right")
 
 root.mainloop()
